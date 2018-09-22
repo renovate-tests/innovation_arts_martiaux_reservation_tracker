5376e319d853751832ef42cc6688944b89f967f9
dcloutier <david.cloutier@oracle.com>
2018-09-22 09:44:41 -0400

    worked on locale switch

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
-  resources :belts
-  resources :students
-  resources :clients
+
+  resources :belts do
+    scope '/:locale' do
+
+    end
+  end
+  resources :students do
+    scope '/:locale' do
+
+    end
+  end
+  resources :clients do
+    scope '/:locale' do
+
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'clients#index'
