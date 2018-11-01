cccd77686ae678d22408a23d0510543a4ff63c6a
dcloutier <david.cloutier@oracle.com>
2018-11-01 12:38:48 -0400

    added sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-    address:              'smtp.sendgrid.net',
-    port:                 587,
-    domain:               'innovationartsmartiaux.com',
-    user_name:            InnovationArtsMartiauxReservationsTracker::Application.credentials.SENDGRID_USERNAME,
-    password:             InnovationArtsMartiauxReservationsTracker::Application.credentials.SENDGRID_PASSWORD,
-    authentication:       'plain',
-    enable_starttls_auto: true
-}
