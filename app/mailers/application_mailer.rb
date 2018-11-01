6f02f95c793ecfeb25eed6fd5812ffd46ff0f3d5
dcloutier <david.cloutier@oracle.com>
2018-11-01 11:45:21 -0400

    added sendgrid

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,15 +2,14 @@ class ApplicationMailer < ActionMailer::Base
   add_template_helper(ApplicationHelper)
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
-  default from: 'no-reply@invalidmail.com'
   ActionMailer::Base.raise_delivery_errors = true
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-      address:              'smtp.gmail.com',
+      address:              'smtp.sendgrid.net',
       port:                 587,
-      domain:               'example.com',
-      user_name:            InnovationArtsMartiauxReservationsTracker::Application.credentials.EMAIL_USER,
-      password:             InnovationArtsMartiauxReservationsTracker::Application.credentials.EMAIL_PASS,
+      domain:               'innovationartsmartiaux.com',
+      user_name:            InnovationArtsMartiauxReservationsTracker::Application.credentials.SENDGRID_USERNAME,
+      password:             InnovationArtsMartiauxReservationsTracker::Application.credentials.SENDGRID_PASSWORD,
       authentication:       'plain',
       enable_starttls_auto: true
   }
