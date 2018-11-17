bf5048d40653f3564bf5871ef337d57f2a03f43a
David Cloutier <david.cloutier@logmein.com>
2018-11-17 11:03:16 -0500

    adding devlopment mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,18 +1,35 @@
 class ApplicationMailer < ActionMailer::Base
   add_template_helper(ApplicationHelper)
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
-  ActionMailer::Base.raise_delivery_errors = true
-  default from: 'innovationartsmartiaux@hotmail.ca'
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-      address:              'smtp.sendgrid.net',
-      port:                 587,
-      domain:               'innovationartsmartiaux.com',
-      password: ENV['SENDGRID_PASSWORD'],
-      user_name: ENV['SENDGRID_USERNAME'],
-      authentication:       'plain',
-      enable_starttls_auto: true
-  }
+  if Rails.env == 'development'
+    ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
+    ActionMailer::Base.raise_delivery_errors = true
+    default from: 'innovationartsmartiaux@hotmail.ca'
+    ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = {
+        address: 'localhost',
+        port: 25,
+        domain: 'innovationartsmartiaux.com',
+        authentication: 'none',
+        enable_starttls_auto: true
+    }
+
+  else
+    ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings[:enable_starttls_auto] = false
+    ActionMailer::Base.raise_delivery_errors = true
+    default from: 'innovationartsmartiaux@hotmail.ca'
+    ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = {
+        address: 'smtp.sendgrid.net',
+        port: 587,
+        domain: 'innovationartsmartiaux.com',
+        password: ENV['SENDGRID_PASSWORD'],
+        user_name: ENV['SENDGRID_USERNAME'],
+        authentication: 'plain',
+        enable_starttls_auto: true
+    }
+  end
+
   layout 'mailer'
 end
